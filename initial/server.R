37685f05426014176b124c7e997d51160eb72324
Harrison Crane <Harrison.Crane@plantandfood.co.nz>
2017-01-26 11:35:28 +1300

    Omissions work now

--- initial/server.R
+++ initial/server.R
@@ -16,6 +16,7 @@ shinyServer(function(input, output, session) {
   controlVar <- reactiveValues(fileUploaded = FALSE, outputTable = TRUE)
   #global table creation
   dat <- omitAllCodesFix <- NULL
+  testParentsVerbaitum <- NULL
   #useful for when we are checking what has been omitted from our table
   rowCount <- currentOmissionCount <- 0
   
@@ -38,7 +39,6 @@ shinyServer(function(input, output, session) {
     currentOmission <<- updateSelectizeInput(session, 'tableFilter', choices = dat$breeder_cross_code)
     if(!is.data.frame(dat))
       return(dat)
-    controlVar$fileUploaded <- TRUE
     start.time <- Sys.time()
     meatAndBeans(dat)
     end.time <- Sys.time()
@@ -53,19 +53,18 @@ shinyServer(function(input, output, session) {
     #now can run the main program
     meatAndBeans(dat)
   })
-  
+
   observeEvent(input$tableFilter, {
     #at any point where your total number of omissions don't match the count of rows in the table you use
     if(length(input$tableFilter) != rowCount){
       #keep omitting rows
-      dat <- subset(dat, !(breeder_cross_code %in% input$tableFilter))
+      dat <- subset(omitAllCodesFix, !(breeder_cross_code %in% input$tableFilter))
     }
     else{
       #reset the table back to pre omission state
       dat <<- omitAllCodesFix
       currentOmission <<- updateSelectizeInput(session, 'tableFilter', choices = dat$breeder_cross_code)
     }
-    #run the main program again
     meatAndBeans(dat)
     return(input$tableFilter)
   })
@@ -76,7 +75,7 @@ shinyServer(function(input, output, session) {
     testParents <- testParents[grep("Green", testParents$Concept), ]
     testParents <- testParents[grep("Destructive", testParents$data_type), ]
     testParents <- testParents[!(is.na(testParents$breeder_cross_code) | testParents$breeder_cross_code==""), ]
-    testParents <- aggregate(harvest_dm ~ breeder_cross_code, data = testParents, FUN = "mean")
+    testParents <- aggregate(harvest_dm ~ breeder_cross_code+Concept+data_type, data = testParents, FUN = "mean")
     testParents$harvest_dm <- round(testParents$harvest_dm, digits = 2)
     #testParents <- cbind(harvest_dm, ~ breeder_cross_code+Concept+data_type, data = dat, FUN = sum)
     #logging the special features of all the various formats of brcrcodes, needed for formatting them in the correct way for output
@@ -87,6 +86,12 @@ shinyServer(function(input, output, session) {
     testParents <- cleanbreeder_cross_codeInFile(testParents, slashCount, underscoreCount, spaceCheck, hyphenCheck)
     #we must make these columns contents as numeric, otherwise we cannot perform math on them
     testParents$harvest_dm <- as.numeric(testParents$harvest_dm)
+    if(controlVar$fileUploaded == FALSE){
+      omitAllCodesFix <<- testParents
+      currentOmission <<- updateSelectizeInput(session, 'tableFilter', choices = omitAllCodesFix$breeder_cross_code)
+      controlVar$fileUploaded == TRUE
+    }
+    print(testParents)
     return(testParents)
   }
   
@@ -94,12 +99,10 @@ shinyServer(function(input, output, session) {
     #for all the rows in our table
     for(i in 1:(dim(x)[1])){
       if(hyphenCheck[i] == 1){
-        print(x$breeder_cross_code[i])
         x$breeder_cross_code[i] <- gsub("-", "_", x$breeder_cross_code[i])
         x$MotherCode[i] <- as.character(str_extract(x$breeder_cross_code[i], "[aA-zZ]+"))
         x$FatherCode[i] <- as.character(str_extract(x$breeder_cross_code[i], "[0-9]+"))
         underscoreCount[i] <- 1
-        print(x$breeder_cross_code[i])
       }
       #if the breeder_cross_code is formatted like this (eg. ZI 343)
       if(spaceCheck[i]==1){
@@ -150,7 +153,7 @@ shinyServer(function(input, output, session) {
     return(x)
   }
   
-  twoDTableCreation <- function(testMothers, testFathers, testParents, testParentsVerbaitum){
+  twoDTableCreation <- function(testMothers, testFathers, testParents){
     #the 2D table is created in here, special techniques must be used, a vector and logging where the survival rate should be indexed
     #make a blank matrix the size of mothercode x fathercode counts
     twoDtable <- matrix(c(""), nrow=length(testMothers$MotherCode), ncol=length(testFathers$FatherCode))
@@ -184,10 +187,10 @@ shinyServer(function(input, output, session) {
         }
       }
       #find the code that matches Nth mother code
-      for(n in 1:length(testParentsVerbaitum$breeder_cross_code)){
+      for(n in 1:length(testParents$breeder_cross_code)){
         #if those two codes match
         if(grepl(testParentsVerbaitum$breeder_cross_code[n], testParents$breeder_cross_code[i])){
-          twoDtable[motherCodeFound, fatherCodeFound] = paste(testParentsVerbaitum$harvest_dm[n])
+          twoDtable[motherCodeFound, fatherCodeFound] = paste(testParents$harvest_dm[n])
           break
         }
       }
@@ -200,10 +203,12 @@ shinyServer(function(input, output, session) {
   
   meatAndBeans <- function(dat){
     #drops unneeded columns and masks brcrcodes with counts for certain special characters, to clean the codes into a single way of input (XX_123) in the best we can
-    testParentsVerbaitum <- testParents <- cleanFile(dat)
-    # drops <- c("MotherCode", "FatherCode")
-    # testParentsVerbaitum <- testParentsVerbaitum[ , !(names(testParentsVerbaitum) %in% drops)]
-    output$x1 = DT::renderDataTable(testParentsVerbaitum, server = FALSE, filter = "bottom")
+    testParents <- cleanFile(dat)
+    if(controlVar$fileUploaded == FALSE){
+      testParentsVerbaitum <<- testParents
+      controlVar$fileUploaded <- TRUE
+    }
+    output$x1 = DT::renderDataTable(testParents, server = FALSE, filter = "bottom")
     #output and download link for combination data table
     output$x5 = downloadHandler('parents-drymatter.csv', content = function(file) {
       s = input$x1_rows_all
@@ -215,46 +220,32 @@ shinyServer(function(input, output, session) {
     testFathers <- select(testParents, FatherCode, harvest_dm)
     testFathers <- aggregate(harvest_dm ~ FatherCode, data = testFathers, FUN = "mean")
     testFathers$harvest_dm <- round(testFathers$harvest_dm, digits = 2)
-    #why cannot print out anything from dat?!
-    #output$overallStats <- renderText(dat)
-    # #let's chuck the same table into all the other output, we'll format them differently in the output
-    # testMothers <- testFathers <- testParentsVerbaitum <- testParents <- aggregate(cbind(Planted, nonPsa, PsaDeaths) ~ BrCrCode+MotherCode+FatherCode+survivalrate, data = testParents, FUN = sum)
-    # #the fix for things like (rOJ _M101) space and underscore in the same code
-    # testParents <- doubleUnderscoreCheck(testParents, testMothers, testFathers, underscoreCount)
-    # #combination table calculation for survival rate
-    # testParentsVerbaitum <- verbatimSanity(testParentsVerbaitum)
-    # #we use the global rowCount variable to check for when we omit everything
-    # rowCount <<- length(testParentsVerbaitum$BrCrCode)
-    # #output and download link for mothers data table
-    # testMothers <- creationTable(testMothers, testParents$MotherCode)
+    #output and download link for mothers data table
     output$x2 = DT::renderDataTable(testMothers, server = FALSE, filter = "bottom")
     output$x6 = downloadHandler('mothers-drymatter.csv', content = function(file) {
       s = input$x2_rows_all
       write.csv(testMothers[s, , drop = FALSE], file)
     })
     # #output and download link for fathers data table
-    # testFathers <- creationTable(testFathers, testParents$FatherCode)
     output$x3 = DT::renderDataTable(testFathers, server = FALSE, filter = "bottom")
     output$x7 = downloadHandler('fathers-drymatter.csv', content = function(file) {
       s = input$x3_rows_all
       write.csv(testFathers[s, , drop = FALSE], file)
     })
     #output and download link for 2D data table
-    twoDtableDF <- twoDTableCreation(testMothers, testFathers, testParents, testParentsVerbaitum)
+    twoDtableDF <- twoDTableCreation(testMothers, testFathers, testParents)
     output$x4 = DT::renderDataTable(twoDtableDF, server = FALSE)
     output$x8 = downloadHandler('overall2dmatrixdrymatter.csv', content = function(file) {
       s = input$x4_rows_all
       write.csv(twoDtableDF[s, , drop = FALSE], file)
     })
-    
   }
   
   #whenever the output changes, we run the function to change the output
   output$contents = renderTable({
-    # if (controlVar$fileUploaded || controlVar$outputTable){
-    #   print(dat)
-    # }
-    meatAndBeans(dat)
+    if (controlVar$fileUploaded){
+      meatAndBeans(dat)
+    }
   })
   
 })
